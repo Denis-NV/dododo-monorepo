9d3ea58bc39e69f82a6e4e447fec23894c01efcb
DenisNV <denisnv@gmail.com>
2025-03-18 07:42:09 +0000

    implemented sst shell and drizzle kit schema generation and migration

--- packages/functions/drizzle.config.ts
+++ packages/functions/drizzle.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "drizzle-kit";
+import { Resource } from "sst";
 
 export default defineConfig({
   dialect: "postgresql", // 'mysql' | 'sqlite' | 'turso'
   schema: ["./src/api/schema.ts"],
   out: "./src/migrations",
+  casing: "snake_case",
+  dbCredentials: {
+    url: `postgresql://${Resource.dododoDatabase.user}:${Resource.dododoDatabase.password}@${Resource.dododoDatabase.host}:${Resource.dododoDatabase.port}/postgres`,
+  },
 });
