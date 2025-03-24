b04d4fb4d4c3187244cd28af9bb0394830643cd5
DenisNV <denisnv@gmail.com>
2025-03-24 06:34:26 +0000

    generated sst types

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,6 +5,30 @@
 
 declare module "sst" {
   export interface Resource {
+    "SupabaseOrgId": {
+      "type": "sst.sst.Secret"
+      "value": string
+    }
+    "dododoApi": {
+      "type": "sst.aws.ApiGatewayV2"
+      "url": string
+    }
+    "dododoAssesment": {
+      "type": "sst.aws.Remix"
+      "url": string
+    }
+    "dododoDatabase": {
+      "database": string
+      "host": string
+      "password": string
+      "port": number
+      "type": "supabase.index/project.Project"
+      "user": string
+    }
+    "dododoStorage": {
+      "name": string
+      "type": "sst.aws.Bucket"
+    }
   }
 }
 /// <reference path="sst-env.d.ts" />
